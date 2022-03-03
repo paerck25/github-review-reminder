f31bbc2153f0e0211049f489ec6b009320ef6c9c
paerck25 <rlaehdwp7451@gmail.com>
2022-03-04 00:26:02 +0900

    fix: github api 함수 수정

--- src/github-api/index.ts
+++ src/github-api/index.ts
@@ -4,49 +4,45 @@ import { UserProfile } from "./types";
 const BASE_URL = "https://api.github.com";
 
 const _apiGet = async (endpoint: string, config?: AxiosRequestConfig) => {
-    const { data } = await axios.get(BASE_URL + endpoint, config);
-    return data;
+    try {
+        const token = localStorage.getItem("token");
+        const { data } = await axios.get(BASE_URL + endpoint, {
+            headers: {
+                Authorization: `token ${token}`
+            },
+            ...config
+        });
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 const _apiPost = async (endpoint: string, config?: AxiosRequestConfig) => {
-    const { data } = await axios.post(BASE_URL + endpoint, config);
+    const token = localStorage.getItem("token");
+    const { data } = await axios.post(BASE_URL + endpoint, {
+        headers: {
+            Authorization: `token ${token}`
+        },
+        ...config
+    });
     return data;
 };
 
 export const getMyUserProfile = async (): Promise<UserProfile | undefined> => {
-    const token = localStorage.getItem("token");
-    return await _apiGet("/user", {
-        headers: {
-            Authorization: `token ${token}`
-        }
-    });
+    return await _apiGet("/user");
 };
 
 export const getMyOrganizationList = async () => {
-    const token = localStorage.getItem("token");
-    return await _apiGet("/user/orgs", {
-        headers: {
-            Authorization: `token ${token}`
-        }
-    });
+    return await _apiGet("/user/orgs");
 };
 
 export const getRepositoriesByOrg = async ({ org }: { org: string }) => {
-    const token = localStorage.getItem("token");
-    return await _apiGet(`/orgs/${org}/repos`, {
-        headers: {
-            Authorization: `token ${token}`
-        }
-    });
+    return await _apiGet(`/orgs/${org}/repos`);
 };
 
 export const getPullRequestsByRepo = async ({ owner, repo }: { owner: string; repo: string }) => {
-    const token = localStorage.getItem("token");
-    return await _apiGet(`/repos/${owner}/${repo}/pulls`, {
-        headers: {
-            Authorization: `token ${token}`
-        }
-    });
+    return await _apiGet(`/repos/${owner}/${repo}/pulls`);
 };
 
 export const getReivewsByPull = async ({
@@ -58,12 +54,7 @@ export const getReivewsByPull = async ({
     repo: string;
     pull_number: number;
 }) => {
-    const token = localStorage.getItem("token");
-    return await _apiGet(`/repos/${owner}/${repo}/pulls/${pull_number}/reviews`, {
-        headers: {
-            Authorization: `token ${token}`
-        }
-    });
+    return await _apiGet(`/repos/${owner}/${repo}/pulls/${pull_number}/reviews`);
 };
 
 export const getReviewersByPull = async ({
@@ -75,10 +66,5 @@ export const getReviewersByPull = async ({
     repo: string;
     pull_number: number;
 }) => {
-    const token = localStorage.getItem("token");
-    return await _apiGet(`/repos/${owner}/${repo}/pulls/${pull_number}/requested_reviewers`, {
-        headers: {
-            Authorization: `token ${token}`
-        }
-    });
+    return await _apiGet(`/repos/${owner}/${repo}/pulls/${pull_number}/requested_reviewers`);
 };
