8b8ff3427a73e693b01506336a4afe7822ee3852
paerck25 <rlaehdwp7451@gmail.com>
2022-03-29 23:19:20 +0900

    fix:5분마다 알람이 울리도록 변경 * 요청 건수 표시 * 새로고침 버튼 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "react-query";
 import styled from "styled-components";
 import ListItem from "../components/ListItem";
 import { fetchPullRequest, getMyUserProfile } from "../github-api";
+import useThrottle from "../hooks/useThrottle";
 const electron = window.require("electron");
 
 export interface Review {
@@ -17,48 +18,63 @@ export interface Review {
 }
 
 const Home = () => {
-    const [reviews, setReviews] = useState<Review[]>([]);
+    const [reviews, setReviews] = useState<Review[] | null>(null);
     const { data: myUserProfile } = useQuery("myUserProfile", getMyUserProfile);
-    const { data: pullRequests } = useQuery("pullRequests", fetchPullRequest);
-
-    useEffect(() => {
-        if (pullRequests) {
-            const my_reviews: Review[] = [];
-            pullRequests.forEach(pr => {
-                const isWating = pr.requested_reviewers.find(reviewr => {
-                    return reviewr.login === myUserProfile?.login;
-                });
-                if (isWating) {
-                    my_reviews.push({
-                        org_name: pr.base.repo.owner.login,
-                        org_avater: pr.base.repo.owner.avatar_url,
-                        repo_name: pr.base.repo.full_name,
-                        pr_title: pr.title,
-                        pr_body: pr.body,
-                        pr_author: pr.user.login,
-                        pr_updated_at: pr.updated_at,
-                        pr_url: pr.html_url
+    const { refetch } = useQuery("pullRequests", fetchPullRequest, {
+        onSuccess: pullRequests => {
+            if (pullRequests) {
+                const my_reviews: Review[] = [];
+                pullRequests.forEach(pr => {
+                    const isWating = pr.requested_reviewers.find(reviewr => {
+                        return reviewr.login === myUserProfile?.login;
                     });
-                }
-            });
-            setReviews(my_reviews);
-        }
-    }, [pullRequests, myUserProfile]);
+                    if (isWating) {
+                        my_reviews.push({
+                            org_name: pr.base.repo.owner.login,
+                            org_avater: pr.base.repo.owner.avatar_url,
+                            repo_name: pr.base.repo.full_name,
+                            pr_title: pr.title,
+                            pr_body: pr.body,
+                            pr_author: pr.user.login,
+                            pr_updated_at: pr.updated_at,
+                            pr_url: pr.html_url
+                        });
+                    }
+                });
+                setReviews(my_reviews);
+            }
+        },
+        enabled: !!myUserProfile,
+        refetchInterval: 5000,
+        refetchOnWindowFocus: true,
+        refetchIntervalInBackground: true
+    });
+
+    const sendNotification = useThrottle(() => {
+        electron.ipcRenderer.send("review_notification", { review_count: reviews?.length || 0 });
+    }, 300000);
 
     useEffect(() => {
-        electron.ipcRenderer.send("review_notification", { review_count: reviews.length });
+        if (reviews) sendNotification();
     }, [reviews]);
 
     const renderReviews = useMemo(
         () =>
-            reviews.map((review, index) => {
+            reviews?.map((review, index) => {
                 return <ListItem key={index} review={review} />;
-            }),
+            }) || [],
         [reviews]
     );
 
+    const onClickRefetch = () => {
+        refetch();
+    };
+
     return (
         <Container>
+            <Header>
+                요청 {renderReviews.length} 건<Refresh onClick={onClickRefetch}>새로고침</Refresh>
+            </Header>
             {renderReviews.length !== 0 ? (
                 renderReviews
             ) : (
@@ -73,13 +89,26 @@ export default Home;
 const Container = styled.div`
     width: 100%;
     height: 100%;
+    display: flex;
+    flex-direction: column;
 `;
 
 const CenterContainer = styled.div`
-    width: 100%;
-    height: 100%;
+    flex: 1;
     display: flex;
     justify-content: center;
     align-items: center;
     color: #57606a;
+`;
+
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    color: #57606a;
+`;
+
+const Refresh = styled.div`
+    cursor: pointer;
 `;
