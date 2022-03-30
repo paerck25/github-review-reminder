0a5f38fb8eb62267db3c05d5f16a879bf47df6a6
paerck25 <rlaehdwp7451@gmail.com>
2022-03-31 01:37:05 +0900

    feat: Sentry 적용

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
 const GlobalStyle = createGlobalStyle`
     *   {
@@ -30,6 +32,12 @@ const GlobalStyle = createGlobalStyle`
         display: none;
     }
 `;
+
+Sentry.init({
+    dsn: "https://a3a4b2d1dadc4114a3a21e736670f07b@o1182893.ingest.sentry.io/6299873",
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0
+});
 
 ReactDOM.render(
     <HashRouter>
